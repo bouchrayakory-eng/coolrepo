d5a2cfc6d8bcfde15975bb61bf4bdc08cc04a766
Bouchraya Yakory <bouchrayakory@gmail.com>
2026-09-07 00:08:36 +0100

    Fix OIDC trust policy using exact job_workflow_ref claim

--- main.tf
+++ main.tf
@@ -117,7 +117,7 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:bouchrayakory-eng/coolrepo:*"
+            "token.actions.githubusercontent.com:job_workflow_ref" = "bouchrayakory-eng/coolrepo/.github/workflows/deploy.yml@*"
           }
         }
       }
